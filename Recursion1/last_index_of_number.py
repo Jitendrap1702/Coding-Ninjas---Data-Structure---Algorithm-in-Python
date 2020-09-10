d3b660a8e0a7ad17ee0593ce5c45f2b6b15f0d7f
Jitendrap1702 <56450901+Jitendrap1702@users.noreply.github.com>
2020-09-10 14:22:02 +0530

    Create last_index_of_number.py

---
+++ Recursion1/last_index_of_number.py
@@ -0,0 +1,18 @@
+def firstIndex(arr,x):
+    l=len(arr)
+    if l==0:
+        return -1
+    if x not in arr:
+        return -1
+    return 1+firstIndex(arr[1:],x)
+    if arr[0]==x:
+        return 0
+    
+    
+
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+print(firstIndex(arr, x))
